36c0800c1aceeb7841c38765b3a33418d42b0877
julienbrg <julien@strat.cc>
2022-10-21 13:54:58 +0200

    add mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,11 +13,11 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    // polygon: {
-    //   url: process.env.POLYGON_URL || "",
-    //   accounts:
-    //     process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    // },
+    mainnet: {
+      url: process.env.MAINNET_URL || "",
+      accounts:
+        process.env.MAINNET_PRIVATE_KEY !== undefined ? [process.env.MAINNET_PRIVATE_KEY] : [],
+    },
   }, 
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
