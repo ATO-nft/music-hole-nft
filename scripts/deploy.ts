925d33d4026e31026f1025afdf66ef156ebc44bb
julienbrg <julien@strat.cc>
2022-10-21 13:54:02 +0200

    clean up

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,18 +11,18 @@ async function main() {
   const royalties = 10 * 100; // 10% resale rights
   const price:any = ethers.utils.parseEther('1') ; // https://bobbyhadz.com/blog/typescript-type-has-no-properties-in-common-with-type
 
-  // const MusicHole = await ethers.getContractFactory("MusicHole");
-  // const mh = await MusicHole.deploy(name, symbol, metadataContent, royalties, price);
-  // await mh.deployed();
+  const MusicHole = await ethers.getContractFactory("MusicHole");
+  const mh = await MusicHole.deploy(name, symbol, metadataContent, royalties, price);
+  await mh.deployed();
 
-  // var msg = color.xterm(39).bgXterm(128);
-  // console.log("NFT contract deployed. ✅", msg(mh.address))
+  var msg = color.xterm(39).bgXterm(128);
+  console.log("NFT contract deployed. ✅", msg(mh.address))
 
   // const addr = "0x0F7CF76d81b9d6984dd43b25cD14253cAB48783d"
 
   // Etherscan verification
   // await mh.deployTransaction.wait(6)
-  await hre.run("verify:verify", { network: "goerli", address: addr, constructorArguments: [name, symbol, metadataContent, royalties, price], });
+  await hre.run("verify:verify", { network: "goerli", address: mh.address, constructorArguments: [name, symbol, metadataContent, royalties, price], });
   console.log("Etherscan verification done. ✅")
   
   console.log("Source code: https://goerli.etherscan.io/address/" + mh.address + "#code")
