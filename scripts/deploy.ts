00ca1cc00baa505738af3440a398eb695e416d5e
julienbrg <julien@strat.cc>
2022-12-07 11:11:44 +0100

    add music hole data

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,9 +6,9 @@ async function main() {
 
   console.log("Deployment started...")
 
-  const name = "Thistle";
-  const symbol = "THISTLE";
-  const metadataContent = "https://bafybeidskqwky4c4rl4ncrcoe3qeybuyrspg5qvfyj3tevh6ceeo3cpayq.ipfs.w3s.link/metatest.json"
+  const name = "Music Hole (2022)";
+  const symbol = "MH";
+  const metadataContent = "https://bafybeih7wfm7pln466yqfy3zct3n4d2kpfqzj5qkua5vck7rhvd4y5ncvy.ipfs.w3s.link/metadata.json"
   const royalties = 10 * 100; // 10% resale rights
   const price:any = ethers.utils.parseEther('0.042') ; // https://bobbyhadz.com/blog/typescript-type-has-no-properties-in-common-with-type
   const max:number = 10000;
@@ -20,15 +20,17 @@ async function main() {
   var msg = color.xterm(39).bgXterm(128);
   console.log("NFT contract deployed. ✅", msg(mh.address))
 
+  console.log("Source code: https://etherscan.io/address/" + mh.address + "#code")
+  console.log("https://ato.network/Ethereum/" + mh.address + "/1")
+  console.log("OpenSea URL: " + "https://opensea.io/asset/ethereum/" + mh.address + "/1")
+
   // Etherscan verification
-  // await mh.deployTransaction.wait(6)
-  // console.log("Verification started...")
-  // await hre.run("verify:verify", { network: "goerli", address: mh.address, constructorArguments: [name, symbol, metadataContent, royalties, price, max], });
+  await mh.deployTransaction.wait(6)
+  console.log("Verification started...")
+  await hre.run("verify:verify", { network: "ethereum", address: mh.address, constructorArguments: [name, symbol, metadataContent, royalties, price, max], });
   console.log("Etherscan verification done. ✅")
   
-  console.log("Source code: https://goerli.etherscan.io/address/" + mh.address + "#code")
-  console.log("https://ato.network/Goerli/" + mh.address + "/1")
-  console.log("OpenSea URL: " + "https://testnets.opensea.io/asset/goerli/" + mh.address + "/1")
+ 
 }
 
 main().catch((error) => {
